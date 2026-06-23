58f8b22953f7c097ab5d26d6cfcb3b3a24e743bd
Riz <rizkiem36@gmail.com>
2026-06-23 19:09:13 +0700

    location

--- MountSyududu.lua
+++ MountSyududu.lua
@@ -1,6 +1,8 @@
-local Window = WindUI:CreateWindow({
-    Title = "My Super Hub",
-    Icon = "door-open", -- lucide icon. optional
-    Author = "by .ftgs and .ftgs", -- optional
-})
---
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer.Character.HumanoidRootPart
+
+local location = CFrame.new(244.37, 53.9, 25.97)
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+wait(0.1)
+player.CFrame = location
